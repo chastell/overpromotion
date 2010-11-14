50522f78ed3c7cd2bdaa227619283a9bf0b95655
Piotr Szotkowski <chastell@chastell.net>
2010-11-14 16:43:29 +0100

    refactor Board#initialize

--- lib/overpromotion/board.rb
+++ lib/overpromotion/board.rb
@@ -5,15 +5,10 @@ module Overpromotion
     attr_reader :rows
 
     def initialize
-      @rows = Array.new(8)
-      @rows.each_index do |i|
-        @rows[i] = []
-        case i
-        when 0, 1 then 8.times { @rows[i] << Stone.new(:white) }
-        when 6, 7 then 8.times { @rows[i] << Stone.new(:black) }
-        else           8.times { @rows[i] << nil }
-        end
-      end
+      @rows = []
+      2.times { @rows << Array.new(8) { Stone.new(:white) } }
+      4.times { @rows << Array.new(8)                       }
+      2.times { @rows << Array.new(8) { Stone.new(:black) } }
     end
 
   end
