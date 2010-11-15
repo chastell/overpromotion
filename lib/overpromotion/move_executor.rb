c004c0439803009ff7d84d23864978010dedc482
Piotr Szotkowski <chastell@chastell.net>
2010-11-15 09:22:05 +0100

    teach MoveExecutor about reactivation

--- lib/overpromotion/move_executor.rb
+++ lib/overpromotion/move_executor.rb
@@ -10,6 +10,10 @@ module Overpromotion
       if MoveValidator.new(@board).valid_move?(player, from, to)
         @board.place_at(to, @board.stone_at(from))
         @board.place_at(from, nil)
+        if (player == :black and to.first > 3) or
+           (player == :white and to.first < 4)
+          @board.stone_at(to).activate!
+        end
         if (player == :black and to.first == 0) or
            (player == :white and to.first == 7)
           @board.stone_at(to).deactivate!
