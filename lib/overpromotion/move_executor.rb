b4a43e298bc63fb87356f78c07e0a8f2663ddecc
Piotr Szotkowski <chastell@chastell.net>
2010-11-15 08:39:08 +0100

    teach MoveExecutor about successful moves

--- lib/overpromotion/move_executor.rb
+++ lib/overpromotion/move_executor.rb
@@ -7,7 +7,13 @@ module Overpromotion
     end
 
     def execute(player, from, to)
-      [@board, :invalid]
+      if MoveValidator.new(@board).valid_move?(player, from, to)
+        @board.place_at(to, @board.stone_at(from))
+        @board.place_at(from, nil)
+        [@board, :successful]
+      else
+        [@board, :invalid]
+      end
     end
 
   end
